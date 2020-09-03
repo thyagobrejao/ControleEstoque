af94b373fed18fcfcf19eff224744946b5391000
Thyago Assunção <thyago.brejao@gmail.com>
2020-09-02 23:12:20 -0300

    Muda tela inicial

--- resources/views/modulo/listagem.blade.php
+++ resources/views/modulo/listagem.blade.php
@@ -1,69 +1,96 @@
 @extends('adminlte::page')
 @section('content')
 <div class="container-fluid">
-  <h1>Listagem Módulos</h1>     
   <div class="row">
     <div class="col-sm-6 col-md-3">
-      <div class="thumbnail">
-        <h2>Venda</h2>
-        <div class="caption">
-          <li class="list-group-item"><a href="{{ route('Cadastrar Venda') }}">Cadastrar Venda</a></li>
-          <li class="list-group-item"><a href="{{ route('Listar Vendas') }}">Listar Vendas</a></li>
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          Vendas
         </div>
-      </div>
-    </div>    
-    <div class="col-sm-6 col-md-3">
-      <div class="thumbnail">
-        <h2>Produto</h2>
-        <div class="caption">
-          <li class="list-group-item"><a href="{{ route('Listar Produtos') }}">Listar Produtos</a></li>
-          <li class="list-group-item"><a href="{{ route('Cadastrar Produto') }}">Cadastrar Produto</a></li>
-        </div>
-      </div>
-    </div>    
-    <div class="col-sm-6 col-md-3">
-      <div class="thumbnail">
-        <h2>Destaque</h2>
-        <div class="caption">
-          <li class="list-group-item"><a href="{{ route('Listar Destaques') }}">Listar Destaques</a></li>
-          <li class="list-group-item"><a href="{{ route('Listar GrupoProdutos') }}">Listar Grupo Produto</a></li>
-        </div>
-      </div>
-    </div>   
-    <div class="col-sm-6 col-md-3">
-      <div class="thumbnail">
-        <h2>Entrada</h2>
-        <div class="caption">
-          <li class="list-group-item"><a href="{{ route('Cadastrar Entrada') }}">Cadastrar Entrada</a></li>
-          <li class="list-group-item"><a href="{{ route('Listar Entradas') }}">Listar Entradas</a></li>
-        </div>
-      </div>
-    </div>    
-    <div class="col-sm-6 col-md-3">
-      <div class="thumbnail">
-        <h2>Saída</h2>
-        <div class="caption">
-          <li class="list-group-item"><a href="{{ route('Listar Saidas') }}">Listar Saídas</a></li>
-        </div>
-      </div>
-    </div>    
-    <div class="col-sm-6 col-md-3">
-      <div class="thumbnail">
-        <h2>Cliente</h2>
-        <div class="caption">
-          <li class="list-group-item"><a href="{{ route('Cadastrar Cliente') }}">Cadastrar Cliente</a></li>
-          <li class="list-group-item"><a href="{{ route('Listar Clientes') }}">Listar Clientes</a></li>
-        </div>
-      </div>
-    </div>    
-    <div class="col-sm-6 col-md-3">
-      <div class="thumbnail">
-        <h2>Relatório</h2>
-        <div class="caption">
-          <li class="list-group-item"><a href="{{ route('Listar Relatorios') }}">Listar Relatorios</a></li>
+        <div class="panel-body">
+          <div class="btn-group-vertical btn-block">
+            <a class="btn btn-info" href="{{ route('Cadastrar Venda') }}">Cadastrar Venda</a>
+            <a class="btn btn-info" href="{{ route('Listar Vendas') }}">Listar Vendas</a>
+          </div>
         </div>
       </div>
     </div>
-  </div> 
+    <div class="col-sm-6 col-md-3">
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          Produto
+        </div>
+        <div class="panel-body">
+          <div class="btn-group-vertical btn-block">
+            <a class="btn btn-info" href="{{ route('Listar Produtos') }}">Listar Produtos</a>
+            <a class="btn btn-info" href="{{ route('Cadastrar Produto') }}">Cadastrar Produto</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-3">
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          Destaque
+        </div>
+        <div class="panel-body">
+          <div class="btn-group-vertical btn-block">
+            <a class="btn btn-info" href="{{ route('Listar Destaques') }}">Listar Destaques</a>
+            <a class="btn btn-info" href="{{ route('Listar GrupoProdutos') }}">Listar Grupo Produto</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-3">
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          Entrada
+        </div>
+        <div class="panel-body">
+          <div class="btn-group-vertical btn-block">
+            <a class="btn btn-info" href="{{ route('Cadastrar Entrada') }}">Cadastrar Entrada</a>
+            <a class="btn btn-info" href="{{ route('Listar Entradas') }}">Listar Entradas</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-3">
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          Saída
+        </div>
+        <div class="panel-body">
+          <div class="btn-group-vertical btn-block">
+            <a class="btn btn-info" href="{{ route('Listar Saidas') }}">Listar Saídas</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-3">
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          Cliente
+        </div>
+        <div class="panel-body">
+          <div class="btn-group-vertical btn-block">
+            <a class="btn btn-info" href="{{ route('Cadastrar Cliente') }}">Cadastrar Cliente</a>
+            <a class="btn btn-info" href="{{ route('Listar Clientes') }}">Listar Clientes</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-3">
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          Relatório
+        </div>
+        <div class="panel-body">
+          <div class="btn-group-vertical btn-block">
+            <a class="btn btn-info"  href="{{ route('Listar Relatorios') }}">Listar Relatorios</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @stop
